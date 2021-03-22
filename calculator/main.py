aeffa633b363550b852dc2c34023880e64c19594
Ali-TM-original <76595455+Ali-TM-original@users.noreply.github.com>
2021-03-22 14:29:50 +0500

    Update main.py

--- calculator/main.py
+++ calculator/main.py
@@ -1,1 +1,22 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.core.window import Window
 
+Window.size = (300, 500)
+Builder.load_file("main.kv")
+
+
+class Mylayout(Widget):
+
+    def clear(self):
+        self.ids.input_bar.text = "0"
+
+
+class Calculator(App):
+    def build(self):
+        return Mylayout()
+
+
+if __name__ == "__main__":
+    Calculator().run()

eef8d11a813fdca79668cd67707d61d6e4ac627e
Ali-TM-original <76595455+Ali-TM-original@users.noreply.github.com>
2021-03-23 07:16:30 +0500

    Update main.py

--- calculator/main.py
+++ calculator/main.py
@@ -12,6 +12,80 @@ class Mylayout(Widget):
     def clear(self):
         self.ids.input_bar.text = "0"
 
+    def button_press(self, num):
+        prior = self.ids.input_bar.text
+
+        if prior == '0':
+            self.ids.input_bar.text = ''
+            self.ids.input_bar.text = f"{num}"
+        else:
+            self.ids.input_bar.text = f"{prior}{num}"
+
+    def add(self):
+        prior = self.ids.input_bar.text
+
+        self.ids.input_bar.text = f"{prior}+"
+
+    def subtraction(self):
+        prior = self.ids.input_bar.text
+
+        self.ids.input_bar.text = f"{prior}-"
+
+    def multiplication(self):
+        prior = self.ids.input_bar.text
+
+        self.ids.input_bar.text = f"{prior}*"
+
+    def division(self):
+        prior = self.ids.input_bar.text
+
+        self.ids.input_bar.text = f"{prior}/"
+
+    def evaluate(self):
+        prior = self.ids.input_bar.text
+
+        if prior == '0':
+            pass
+        else:
+            try:
+                x = eval(prior)
+                self.ids.input_bar.text = f"{x}"
+            except ZeroDivisionError:
+                self.ids.input_bar.text = '0'
+            except SyntaxError:
+                self.ids.input_bar.text = '0'
+
+    def decimal(self):
+        prior = self.ids.input_bar.text
+        self.ids.input_bar.text = f"{prior}."
+
+    def percent(self):
+        prior = self.ids.input_bar.text
+
+        if prior == "0":
+            pass
+        else:
+            prior = f'{prior}/100'
+            x = eval(prior)
+            self.ids.input_bar.text = f"{x}"
+
+    def negatize(self):
+        prior = self.ids.input_bar.text
+
+        if prior == "0":
+            pass
+        else:
+            self.ids.input_bar.text = f"-{prior}"
+
+    def test_delete(self):
+        prior = self.ids.input_bar.text
+
+        if prior == "0":
+            pass
+        else:
+            prior = prior[:-1]
+            self.ids.input_bar.text = f'{prior}'
+
 
 class Calculator(App):
     def build(self):
